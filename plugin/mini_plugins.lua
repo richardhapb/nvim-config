2c283814886096e2f65c7a7073b9d7f694730eb6
richardhapb <richard.hapb@icloud.com>
2025-02-18 01:50:40 -0300

    feat: neospeller as a plugin

--- plugin/mini_plugins.lua
+++ plugin/mini_plugins.lua
@@ -1,4 +1,4 @@
-local plugins = { 'FormatDicts', 'LatexPreview', 'marp', 'mermaid', 'sqlquery', 'neospeller' }
+local plugins = { 'FormatDicts', 'LatexPreview', 'marp', 'mermaid', 'sqlquery' }
 
 for _, plugin in ipairs(plugins) do
    require('plugin.' .. plugin).setup()
