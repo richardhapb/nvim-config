1cf7282f886eb0978ee4697d02ac6d585ab0813a
richardhapb <richard.hapb@icloud.com>
2025-04-21 01:35:42 -0400

    feat: disable retab for go

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -1,50 +1,55 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
-   group = vim.api.nvim_create_augroup("RetabOnSave", {clear = true}),
-   callback = function()
+  group = vim.api.nvim_create_augroup("RetabOnSave", { clear = true }),
+  callback = function(args)
+    local ft = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
+    if ft ~= "go" then
       vim.cmd('retab!')
-   end
+    end
+  end
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-   group = vim.api.nvim_create_augroup("YankConfig", {clear = true}),
-   pattern = "*",
-   callback = function()
-      vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
-   end,
+  group = vim.api.nvim_create_augroup("YankConfig", { clear = true }),
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+  end,
 })
 
 vim.api.nvim_create_autocmd("Filetype", {
-   group = vim.api.nvim_create_augroup("FugitiveConfig", {clear = true}),
-   pattern = "fugitive",
-   callback = function()
-      local keymap = vim.keymap.set
+  group = vim.api.nvim_create_augroup("FugitiveConfig", { clear = true }),
+  pattern = "fugitive",
+  callback = function()
+    local keymap = vim.keymap.set
 
-      keymap('n', '<leader>P', '<cmd>Git push<cr>', { noremap = true, desc = "Git push", buffer = 0 })
-      keymap('n', '<leader>F', '<cmd>Git push --force-with-lease<cr>', { noremap = true, desc = "Git push force with lease", buffer = 0 })
-      keymap('n', '<leader>p', '<cmd>Git pull --rebase<cr>', { noremap = true, desc = "Git pull", buffer = 0 })
-   end
+    keymap('n', '<leader>P', '<cmd>Git push<cr>', { noremap = true, desc = "Git push", buffer = 0 })
+    keymap('n', '<leader>F', '<cmd>Git push --force-with-lease<cr>',
+      { noremap = true, desc = "Git push force with lease", buffer = 0 })
+    keymap('n', '<leader>p', '<cmd>Git pull --rebase<cr>', { noremap = true, desc = "Git pull", buffer = 0 })
+  end
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-   group = vim.api.nvim_create_augroup('LuaTest', {clear = true}),
-   pattern = 'lua',
-   callback = function()
-      vim.keymap.set('n', '<leader>tf', '<cmd>write<cr><cmd>PlenaryBustedFile %<cr>', { noremap = true, desc = 'Run tests' })
-   end
+  group = vim.api.nvim_create_augroup('LuaTest', { clear = true }),
+  pattern = 'lua',
+  callback = function()
+    vim.keymap.set('n', '<leader>tf', '<cmd>write<cr><cmd>PlenaryBustedFile %<cr>',
+      { noremap = true, desc = 'Run tests' })
+  end
 })
 
 vim.api.nvim_create_autocmd("TermOpen", {
-   group = vim.api.nvim_create_augroup("TermConfig", {clear = true}),
-   callback = function()
-      vim.opt_local.number = false
-      vim.opt_local.relativenumber = false
-      vim.bo.filetype = "terminal"
-   end
+  group = vim.api.nvim_create_augroup("TermConfig", { clear = true }),
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.bo.filetype = "terminal"
+  end
 })
 
 -- Avoid jsonls attached in a new jupyter notebook
 vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("JupyterConfig", {clear = true}),
+  group = vim.api.nvim_create_augroup("JupyterConfig", { clear = true }),
   callback = function(args)
     if args.file:find('%.ipynb$') then
       vim.bo[args.buf].filetype = "python"
