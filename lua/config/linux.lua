207f2c44248f8f84846d96f36ed17bf798dedf73
richardhapb <richard.hapb@icloud.com>
2025-04-23 01:24:26 -0400

    feat(config): comment out Raspberry Pi clipboard config
    The Raspberry Pi-specific clipboard configuration has been commented
    out to prevent its execution. This change ensures compatibility with
    systems where the `utils.is_raspberry_pi()` function is not required
    or available.
    
    - Commented out `utils.is_raspberry_pi()` check and related clipboard
      configuration.
    - Retained the `vim.opt.clipboard` setting for general use.

--- lua/config/linux.lua
+++ lua/config/linux.lua
@@ -1,19 +1,19 @@
-local utils = require 'functions.utils'
+-- local utils = require 'functions.utils'
 
 vim.opt.clipboard:append { "unnamedplus" }
 
-if utils.is_raspberry_pi() then
-  --- Use the server to pass the clipboard through SSH
-  vim.g.clipboard = {
-    name = "ssh-clipboard",
-    copy = {
-      ['+'] = { 'nc', '-q0', 'localhost', '2224' },
-      ['*'] = { 'nc', '-q0', 'localhost', '2224' },
-    },
-    paste = {
-      ['+'] = { 'nc', '-d', 'localhost', '2225' },
-      ['*'] = { 'nc', '-d', 'localhost', '2225' },
-    },
-    cache_enabled = 1
-  }
-end
+-- if utils.is_raspberry_pi() then
+--   --- Use the server to pass the clipboard through SSH
+--   vim.g.clipboard = {
+--     name = "ssh-clipboard",
+--     copy = {
+--       ['+'] = { 'nc', '-q0', 'localhost', '2224' },
+--       ['*'] = { 'nc', '-q0', 'localhost', '2224' },
+--     },
+--     paste = {
+--       ['+'] = { 'nc', 'localhost', '2225' },
+--       ['*'] = { 'nc', 'localhost', '2225' },
+--     },
+--     cache_enabled = 1
+--   }
+-- end
