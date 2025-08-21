80e43d6dd7ded17c9055d2c9eb92e2f4d404fad6
richardhapb <richard.hapb@icloud.com>
2025-08-21 17:38:04 -0400

    feat(aligner): add text alignment plugin
    - Implement aligner.lua for aligning text by a given character in visual range
    - Register Align user command for in-buffer alignment
    - Update mini_plugins.lua to load aligner plugin

---
+++ lua/plugin/aligner.lua
@@ -0,0 +1,60 @@
+local M = {}
+
+---@param char string
+---@param text string[]
+---@return string[]
+local function align(char, text)
+  local max = 0
+  local lefts = {}
+  local rights = {}
+
+  for _, line in ipairs(text) do
+    local l, r = unpack(vim.split(line, char, { plain = true }))
+
+    if #l > max then
+      max = #l
+    end
+
+    table.insert(lefts, l)
+    table.insert(rights, r)
+  end
+
+  local result = {}
+
+  for i, l in ipairs(lefts) do
+    if #l < max then
+      l = string.format("%s%s", l, string.rep(" ", max - #l))
+    end
+    if rights[i] then
+      table.insert(result, string.format("%s%s%s", l, char, rights[i]))
+    else
+      table.insert(result, vim.trim(l))
+    end
+  end
+
+  return result
+end
+
+function M.setup()
+  vim.api.nvim_create_user_command('Align',
+    function(args)
+      local line1 = 1
+      local line2 = -1
+      if args.range == 0 then
+        return
+      end
+
+      line1 = args.line1
+      line2 = args.line2
+
+      local lines = vim.api.nvim_buf_get_lines(0, line1 - 1, line2, false)
+      local new_text = align(vim.trim(args.args), lines)
+      vim.api.nvim_buf_set_lines(0, line1 - 1, line2, false, new_text)
+    end, {
+      nargs = 1,
+      range = 1
+    })
+end
+
+return M
+
--- plugin/mini_plugins.lua
+++ plugin/mini_plugins.lua
@@ -1,4 +1,4 @@
-local plugins = { 'FormatDicts', 'LatexPreview', 'marp', 'mermaid', 'sqlquery', 'jn_watcher', "executor", "copilot" }
+local plugins = { 'FormatDicts', 'LatexPreview', 'marp', 'mermaid', 'sqlquery', 'jn_watcher', "executor", "copilot", "aligner" }
 
 for _, plugin in ipairs(plugins) do
    require('plugin.' .. plugin).setup()
