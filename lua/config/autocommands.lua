94cb579f46862ca7ae06cffc8041953ee3b3ec4e
richardhapb <richard.hapb@icloud.com>
2026-06-17 01:38:29 -0400

    update deprecated on_yank

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("YankConfig", { clear = true }),
   pattern = "*",
   callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+    vim.hl.hl_op({ higroup = "IncSearch", timeout = 200 })
   end,
 })
 
